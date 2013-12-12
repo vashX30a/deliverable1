88d8ba2015b6daa3f79192c7a8ec438ab410946b
VashX30a <vashX30a@gmail.com>
2013-12-12 16:24:17 +0800

    start new address book

---
+++ addressBookCSC184/src/del1/Contact.java
@@ -0,0 +1,11 @@
+package del1;
+
+public class Contact {
+	Contact(){
+		fName = lName = address = city = state = pNumber = null;
+		zipCode = 0000; 
+	}
+	
+	private	String fName, lName, address, city, state, pNumber;
+	private	int zipCode;
+}
